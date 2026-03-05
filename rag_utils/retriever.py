5c464ccd00bf9b907ce44197c6bb4ebf94f45b33
microrageupgradevipchatbot-alt <microrageupgradevipchatbot@gmail.com>
2026-03-05 13:07:52 +0500

    Update retriever.py

--- rag_utils/retriever.py
+++ rag_utils/retriever.py
@@ -1,18 +1,18 @@
 from .setup import logger
-def retrieve_docs(query,chromaDB):
+def retrieve_docs(query,faiss_db):
   '''Retrieve Docs from Vector Store using similarity search'''
-  retrieveDocs = chromaDB.similarity_search(query,k=2)
+  retrieveDocs = faiss_db.similarity_search(query, k=2)
 
   return retrieveDocs
 def get_context_from_docs(documents):
   '''Get pag_content from documents to create context'''
-
+                          
   context = "\n\n".join([doc.page_content for doc in documents])
   for i, doc in enumerate(documents):
       logger.info(f"📋 Chunk {i+1}: {doc.page_content}")
   return context
 
-def get_context(query,chromaDB):
-    retrieved_docs = retrieve_docs(query,chromaDB)
+def get_context(query,faiss_db):
+    retrieved_docs = retrieve_docs(query,faiss_db)
     context= get_context_from_docs(retrieved_docs)
     return context
